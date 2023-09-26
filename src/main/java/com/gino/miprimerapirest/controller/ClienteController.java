134c6c9bec4677919c0b6d7371160aeaa1a83528
Gino Paoletti Geuna <ginopaolettigeuna30@gmail.com>
2023-09-26 15:54:19 -0300

    Controlador api

--- src/main/java/com/gino/miprimerapirest/controller/ClienteController.java
+++ src/main/java/com/gino/miprimerapirest/controller/ClienteController.java
@@ -1,4 +1,30 @@
 package com.gino.miprimerapirest.controller;
 
+import com.gino.miprimerapirest.model.entity.Cliente;
+import com.gino.miprimerapirest.service.ICliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
 public class ClienteController {
+    @Autowired
+    private ICliente clienteService;
+
+    @PostMapping("cliente")
+    public Cliente create(@RequestBody Cliente cliente){
+        return clienteService.save(cliente);
+    }
+    @PutMapping("cliente")
+    public Cliente update(@RequestBody Cliente cliente){
+        return clienteService.save(cliente); //no es un update solo guarda el cliente en la bd
+    }
+    @DeleteMapping("cliente/{id}")
+    public void delete(@PathVariable int id){
+       clienteService.delete(clienteService.foundById(id));
+    }
+    @GetMapping("cliente/{id}")
+    public Cliente showById(@PathVariable int id){
+        return clienteService.foundById(id);
+    }
 }
